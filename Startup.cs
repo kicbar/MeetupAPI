7704940224e38f7c54a3b0c38d1d127958a3cb9b
kicbar <bartlomiejkicior@gmail.com>
2020-12-27 16:38:56 +0100

    add swagger

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace MeetupAPI
 {
@@ -18,11 +19,21 @@ namespace MeetupAPI
             services.AddDbContext<MeetupContext>();
             services.AddScoped<MeetupSeeder>();
             services.AddAutoMapper(this.GetType().Assembly);
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("MeetupAPI", new OpenApiInfo() { Title = "MeetupAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MeetupSeeder meetupSeeder)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI( c => 
+            {
+                c.SwaggerEndpoint("/swagger/MeetupAPI/swagger.json", "MeetupAPI-BKversion");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
