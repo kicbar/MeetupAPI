13af961be5a95ba8eb865cc313a75fd47134ea26
kicbar <bartlomiejkicior@gmail.com>
2020-12-27 13:02:40 +0100

    add include Lectures in MeetupController - Get method

--- Controllers/MeetupController.cs
+++ Controllers/MeetupController.cs
@@ -35,6 +35,7 @@ namespace MeetupAPI.Controllers
         {
             var meetup = _meetupContext.Meetups
                 .Include(m => m.Location)
+                .Include(m => m.Lectures)
                 .FirstOrDefault(m => m.Name.Replace(" ", "-").ToLower() == meetupName.ToLower());
             if (meetup == null)
                 return NotFound();
